9f9adb052d6f8230d0ec7e3775021731628dae09
Charbel Jacquin <charbel.jacquin@gmail.com>
2021-02-17 10:03:08 +0100

    add db_create(), db_drop()

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,29 @@
+extern crate base64;
 extern crate dotenv;
-extern crate serde_json;
 extern crate jsonrpc_client_core;
 extern crate jsonrpc_client_http;
-extern crate base64;
+extern crate serde_json;
 use dotenv::dotenv;
 use std::env;
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufWriter;
-use std::fs::File;
 
-pub use serde_json::{Value, Map, Number};
 pub use serde_json::json;
+pub use serde_json::{Map, Number, Value};
 
 use jsonrpc_client_core::{call_method, Error};
-use std::io::Read;
-use std::io::Cursor;
 use jsonrpc_client_http::HttpHandle;
 use jsonrpc_client_http::HttpTransport;
+use std::io::Cursor;
 
 #[derive(Default, Debug)]
 struct Odoo {
     host: String,
     port: u32,
     protocol: String,
-    version: String
+    version: String,
 }
 
 const ODOO_LOGIN: &str = "http://localhost:8069/web/session/authenticate";
@@ -32,9 +31,9 @@ const ODOO_LOGOUT: &str = "http://localhost:8069/web/session/destroy";
 const ODOO_JSONRPC: &str = "http://localhost:8069/jsonrpc";
 
 pub struct OdooApi {
-  login_handle: HttpHandle,
-  logout_handle: HttpHandle,
-  jsonrpc_handle: HttpHandle,
+    login_handle: HttpHandle,
+    logout_handle: HttpHandle,
+    jsonrpc_handle: HttpHandle,
 }
 
 impl OdooApi {
@@ -50,24 +49,79 @@ impl OdooApi {
         }
     }
     pub fn login(&mut self, db: &str, login: &str, password: &str) -> Value {
-        call_method(&mut self.login_handle, "call".to_owned(), json!({"db": db, "login": login, "password": password})).call().unwrap()
+        call_method(
+            &mut self.login_handle,
+            "call".to_owned(),
+            json!({"db": db, "login": login, "password": password}),
+        )
+        .call()
+        .unwrap()
     }
     pub fn logout(&mut self) -> Value {
-        call_method(&mut self.logout_handle, "call".to_owned(), json!({})).call().unwrap()
+        call_method(&mut self.logout_handle, "call".to_owned(), json!({}))
+            .call()
+            .unwrap()
     }
     pub fn db_list(&mut self) -> Value {
-        call_method(&mut self.jsonrpc_handle, "call".to_owned(), json!({
-            "service": "db",
-            "method": "list",
-            "args": []
-        })).call().unwrap()
+        call_method(
+            &mut self.jsonrpc_handle,
+            "call".to_owned(),
+            json!({
+                "service": "db",
+                "method": "list",
+                "args": []
+            }),
+        )
+        .call()
+        .unwrap()
     }
-    pub fn db_dump(&mut self, master_password: &str, db: &str, format: &str) -> Result<Value, Error> {
-        call_method(&mut self.jsonrpc_handle, "call".to_owned(), json!({
-            "service": "db",
-            "method": "dump",
-            "args": [master_password, db, format]
-        })).call()
+    pub fn db_dump(
+        &mut self,
+        master_password: &str,
+        db: &str,
+        format: &str,
+    ) -> Result<Value, Error> {
+        call_method(
+            &mut self.jsonrpc_handle,
+            "call".to_owned(),
+            json!({
+                "service": "db",
+                "method": "dump",
+                "args": [master_password, db, format]
+            }),
+        )
+        .call()
+    }
+    pub fn db_create(
+        &mut self,
+        master_password: &str,
+        db: &str,
+        demo: bool,
+        lang: &str,
+        admin_password: &str,
+    ) -> Result<Value, Error> {
+        call_method(
+            &mut self.jsonrpc_handle,
+            "call".to_owned(),
+            json!({
+                "service": "db",
+                "method": "create_database",
+                "args": [master_password, db, demo, lang, admin_password]
+            }),
+        )
+        .call()
+    }
+    pub fn db_drop(&mut self, master_password: &str, db: &str) -> Result<Value, Error> {
+        call_method(
+            &mut self.jsonrpc_handle,
+            "call".to_owned(),
+            json!({
+                "service": "db",
+                "method": "drop",
+                "args": [master_password, db]
+            }),
+        )
+        .call()
     }
 }
 
@@ -76,7 +130,7 @@ impl Odoo {
         println!("login: db: {}, u: {}, pwd: {}", db, user, password);
     }
 }
-fn main () -> io::Result<()> {
+fn main() -> io::Result<()> {
     dotenv().ok();
     for (key, env) in env::vars() {
         println!("{}: {}", key, env);
@@ -91,12 +145,12 @@ fn main () -> io::Result<()> {
             Ok(val) => val,
             Err(_) => "localhost".to_owned(),
         },
-        port: port, 
+        port: port,
         protocol: match port {
             443 => "jsonrpc+ssl".to_owned(),
-            _ => "jsonrpc".to_owned()
+            _ => "jsonrpc".to_owned(),
         },
-        version: "0.9".to_owned()
+        version: "0.9".to_owned(),
     };
     println!("Odoo: {:?}", odoo);
     odoo.login("tec-528".to_owned(), "admin".to_owned(), "admin".to_owned());
@@ -116,28 +170,52 @@ fn main () -> io::Result<()> {
     let res = client.db_dump("diabeloop", "tec-528", "zip");
     match res {
         Ok(Value::String(val)) => {
-            println!("decoding data: {:#?}", &val[0..1000]);
-            let mut wrapped_reader = Cursor::new(val);
-            let mut decoder = base64::read::DecoderReader::new(
-                &mut wrapped_reader, base64::STANDARD
-            );
-            let mut result = Vec::new();
-            decoder.read_to_end(&mut result).unwrap();
-            println!("save file ...");
+            // println!("decoding data:\n{:#?}", &val[0..1000]);
             let f = File::create("dump.zip")?;
-            {
-                let mut writer = BufWriter::new(f);
-                writer.write(&result)?;
+            let mut writer = BufWriter::new(f);
+            let wrapped_reader = Cursor::new(val);
+            println!("save file ...");
+            for line in wrapped_reader.lines() {
+                //let data = base64::decode(line.as_bytes()).unwrap();
+                match line {
+                    Ok(val) => {
+                        let data = base64::decode(val).unwrap();
+                        writer.write(&data)?;
+                    }
+                    Err(err) => {
+                        println!("err: {:#?}", err);
+                    }
+                };
             }
-            Ok(())
-        },
+            //let data = base64::decode(val).unwrap();
+            println!("done.");
+        }
         Ok(_) => {
             println!("Huu");
-            Err(std::io::Error::new(std::io::ErrorKind::Other, "oh no!"))
-        },
+        }
         Err(err) => {
             println!("error: {:#?}", err);
-            Err(std::io::Error::new(std::io::ErrorKind::Other, "oh no no no!!!"))
-        },
+        }
     }
+    println!("db create ...");
+    let res = client.db_create("diabeloop", "test2", false, "fr_FR", "admin");
+    match res {
+        Ok(val) => {
+            println!("create: {:#?}", val);
+        }
+        Err(err) => {
+            println!("error: {:#?}", err);
+        }
+    };
+    println!("db drop ...");
+    let res = client.db_drop("diabeloop", "test2");
+    match res {
+        Ok(val) => {
+            println!("drop: {:#?}", val);
+        }
+        Err(err) => {
+            println!("err: {:#?}", err);
+        }
+    };
+    Ok(())
 }
